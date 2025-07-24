7ba6f96349dab2c9fd04697192162c88a08ef0b6
OcampoPythonZen <eocampo1986@gmail.com>
2025-07-23 21:12:19 -0600

    time and space complexity crunchyRollChallenge code challenge

--- src/algorithms/TimeAndSpaceComplexity.java
+++ src/algorithms/TimeAndSpaceComplexity.java
@@ -1,24 +1,65 @@
 package algorithms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class TimeAndSpaceComplexity {
 
+    public static void main(String[] args) {
+        String[][] records = {
+                {"Edgar", "exit"},
+                {"Paul", "enter"},
+                {"Paul", "exit"},
+                {"John", "enter"},
+                {"Paul", "enter"},
+                {"Paul", "exit"},
+                {"John", "exit"},
+                {"Maria", "exit"},
+                {"Annie", "enter"},
+        };
+        Result res = crunchyRollChallenge(records);
+        System.out.println("No Exit: " + res.noExit);
+        System.out.println("No Entry: " + res.noEntry);
+    }
 
-    public static void howManyTimesInArray(int[] arr) {
-        Map<Integer, Integer> times = new HashMap<>();
-        for (Integer element : arr) {
-            times.put(element, times.getOrDefault(element, 0) + 1);
-        }
-        for (Map.Entry<Integer, Integer> entry : times.entrySet()) {
-            System.out.println("value: " + entry.getKey() + " in " + entry.getValue() + " time(s).");
+
+    public static class Result {
+        List<String> noExit;
+        List<String> noEntry;
+
+        public Result(List<String> noExit, List<String> noEntry) {
+            this.noExit = noExit;
+            this.noEntry = noEntry;
         }
     }
 
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 2, 3, 3, 3};
-        howManyTimesInArray(arr);
+    public static Result crunchyRollChallenge(String[][] records) {
+        Map<String, Integer> balance = new HashMap<>();
+        Set<String> noEntry = new HashSet<>();
+        List<String> noExit = new ArrayList<>();
 
+        for (String[] record : records) {
+            String name = record[0];
+            String status = record[1];
+            if (status.equals("enter")) {
+                balance.put(name, balance.getOrDefault(name, 0) + 1);
+            } else if (status.equals("exit")) {
+                if (balance.getOrDefault(name, 0) == 0) {
+                    noEntry.add(name);
+                } else {
+                    balance.put(name, balance.get(name) - 1);
+                }
+            }
+        }
+        for (Map.Entry<String, Integer> entry : balance.entrySet()) {
+            if (entry.getValue() > 0) {
+                noExit.add(entry.getKey());
+            }
+        }
+        return new Result(noExit, new ArrayList<>(noEntry));
     }
 }
